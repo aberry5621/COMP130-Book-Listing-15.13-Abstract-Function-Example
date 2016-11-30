9b8613d7482cb134b2f3b8da7a38e0a0ff711a20
aberry5621 <aberry5621@gmail.com>
2016-11-30 10:55:51 -0800

    Initial Commit

---
+++ COMP130 Book Listing 15.13 Abstract Function Example/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP130 Book Listing 15.13 Abstract Function Example
+//
+//  Created by ax on 11/30/16.
+//  Copyright © 2016 COMP130. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
